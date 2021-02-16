1cfdc57b9436116f6497af810d1f1747fe887ae2
MrTir0 <70948323+MrTir0@users.noreply.github.com>
2021-02-16 16:23:10 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'baglan.php';
+
+if (!isset($_GET['sayfa']))
+    {
+        $_GET['sayfa'] = 'index';
+    }
+
+    switch ($_GET['sayfa']){ 
+
+        case 'insert':
+            require_once 'insert.php';
+        break;
+    }
+
+?>
